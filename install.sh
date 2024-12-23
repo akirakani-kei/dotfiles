b3e6cbca062088fe5723cc2316cab6f59d092f94
kei <52973114+akirakani-kei@users.noreply.github.com>
2024-12-23 23:48:00 +0200

    installation script

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ echo "Installing required packages/fonts ONLY works if you're on Arch or an Arch
 read -r -p "Would you like to install ALL packages required for the configuration? (listed in README) [y/N]: " response
 response=$(echo "$response" | tr '[:upper:]' '[:lower:]')
 if [ "$response" = "y" ] || [ "$response" = "yes" ]; then
-    sudo pacman -S bspwm xorg-server xorg-xinit xorg-xsetroot playerctl pamixer sxhkd polybar picom thunar ranger zathura zathura-pdf-poppler alacritty dmenu alacritty nitrogen bashtop
+    sudo pacman -S bspwm xorg-server xorg-xinit xorg-xsetroot playerctl pamixer sxhkd polybar picom thunar ranger zathura zathura-pdf-poppler kitty dmenu nitrogen bashtop
 fi
 
 read -r -p "Would you like to install required fonts? [y/N]: " response
@@ -16,7 +16,7 @@ if [ ! -d "$HOME/.config" ]; then
   mkdir -p "$HOME/.config"
 
 cd ~/.config
-mkdir bspwm sxhkd zathura polybar alacritty nvim
+mkdir bspwm sxhkd zathura polybar kitty nvim
 
 git clone https://github.com/NvChad/starter nvim
 
@@ -25,7 +25,7 @@ cd dotfiles
 mv .xinitrc ~
 cd ~/.config
 mv dotfiles/bspwmrc bspwm
-mv dotfiles/alacritty.toml alacritty
+mv dotfiles/kitty.conf kitty
 mv dotfiles/sxhkd/sxhkdrc sxhkd
 mv dotfiles/sxhkd/rewind.sh sxhkd
 mv dotfiles/zathurarc zathura
@@ -39,7 +39,7 @@ chmod +x ~/.config/sxhkd/rewind.sh
 chmod +x ~/.config/polybar/launch.sh
 chmod +x ~/.config/polybar/config
 chmod +x ~/.config/zathura/zathurarc
-chmod +x ~/.config/alacritty/alacritty.toml
+chmod +x ~/.config/kitty/kitty.conf
 
 rm -rf ~/.config/dotfiles
 
